beca927467c9fd90cb1c083ddc0b6e9e7c907eba
timofeeva-olya <olya.sinyakova.20.02@mail.ru>
2024-09-20 13:14:39 +0300

    Add files via upload

---
+++ fake_math.py
@@ -0,0 +1,8 @@
+def divide (first, second):
+    if second == 0:
+        print("Ошибка")
+    else:
+        return first / second
+
+r=divide(5,3)
+print(r)
---
+++ true_math.py
@@ -0,0 +1,9 @@
+def divide (first, second):
+    if second == 0:
+        print("Бесконечность")
+    else:
+        return first / second
+
+
+r = divide(5, 0)
+print(r)
